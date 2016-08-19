6aef5ee95860d9ccdbd2670a600dfecb2acf38e5
Gennadiy Remizov <gremov@gmail.com>
2016-08-19 12:51:12 +0300

    calculator styled

--- calc/js/main.js
+++ calc/js/main.js
@@ -1,3 +1,5 @@
+/* Calculator algorithm */
+
 window.onload = function () {
     var buttons = document.getElementsByTagName('span'),
         result = document.querySelectorAll('.result p')[0],
@@ -34,6 +36,11 @@ window.onload = function () {
     };
 };
 
+
+
+
+/* Calculator show/hide button actions*/
+
 $(function() {
     var calculator = $('.calculator'),
         buttonLink = $('.container .calc-button'),
@@ -64,36 +71,4 @@ $(function() {
         e.stopPropagation();
     });
 });
-// $('.calc-button').click(function() {
-//     $( "#calculator" ).show( "slow" );
-// });
-
-// $("body").click(function(e) {
-//     if($(e.target).closest(".calculator .active").length==0)
-//         $(".calculator .active").css("display","none");
-// });
-
-
-
-// $('body').click(hideCalculator);
-//
-// function hideCalculator() {
-//     var calculator2 = document.getElementsByClassName("calculator");
-//     calculator2.style.display = "none";
-// };
-
-// var showCalculatorButton = document.getElementById("calc-button");
-// showCalculatorButton.addEventListener("click", showCalculator);
-
-// document.onclick = function () {
-//     // document.getElementById('nav').style.display = 'none'
-//     var hideCalculator = document.getElementById("calculator");
-//     hideCalculator.style.display = "none";
-// }
-// function hideCalculator() {
-//     var calculator = document.getElementById("calculator");
-//     calculator.style.display = "none";
-// };
-// var hideCalculatorButton = document.getElementById("body");
-// hideCalculatorButton.addEventListener("click", hideCalculator);
 
